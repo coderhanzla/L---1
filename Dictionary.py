a42239d890b139665132f615c1bd6197323b6457
coderhanzla@gmail.com <coderhanzla@gmail.com>
2024-11-23 19:54:19 +0530

    Operations On Dictionary

---
+++ Dictionary.py
@@ -0,0 +1,16 @@
+dict = {}
+print(dict)
+
+Dictionary = {"Name": "Hanzla" , "Age": 11 , "School": "Ryan International School"}
+print(Dictionary)
+
+Dictionary["Age"] = 12
+print(Dictionary)
+
+Dictionary["Roll No:"] = "15"
+print(Dictionary)
+
+print(Dictionary.get("School"))
+
+Dictionary.clear()
+print(Dictionary)
