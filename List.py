4af4320b35707f557b39760748d7f83bac070d42
coderhanzla@gmail.com <coderhanzla@gmail.com>
2024-11-23 19:36:39 +0530

    Operation on lists

---
+++ List.py
@@ -0,0 +1,27 @@
+List0 = [ ]
+
+print(List0)
+
+List1 = ["Hanzla" , 18 , "Coder"]
+print(List1)
+
+List1.append("Codingal Student")
+print(List1)
+
+List1.remove(18)
+print(List1)
+
+List1.sort()
+print(List1)
+
+List1.reverse()
+print(List1)
+
+
+
+List1.clear()
+print(List1)
+
+
+
+
